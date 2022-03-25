fe2eca29a7b4dd13c28a5d5ef0a60d9aa9544979
Waritz <wrtsr.mm@hotmail.com>
2022-03-26 05:48:30 +0700

    mamiaw

--- app_amonze/views.py
+++ app_amonze/views.py
@@ -2,13 +2,16 @@ from audioop import reverse
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from app_amonze.models import Item
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def marketplace(request):
-    return render(request, 'marketplace.html')
+    items = Item.objects.all()
+    context = { 'items':items }
+    return render(request, 'marketplace.html', context)
 
 def item(request, item_id):
     return render(request, 'item.html', context={'item_id': item_id})
