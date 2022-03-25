22f6c2efc9f318d77a9085239e04644546f6ff9f
Nattawut Chamcheun <nattawutchamcheun@gmail.com>
2022-03-26 00:52:15 +0700

    friend

--- project_amonze/urls.py
+++ project_amonze/urls.py
@@ -24,3 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
