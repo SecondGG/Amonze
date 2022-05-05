82a6f78677d6a227ff719bc948468b9bde491939
Nattawut Chamcheun <93769837+SecondGG@users.noreply.github.com>
2022-05-05 15:52:40 +0700

    remove secret key

--- project_amonze/settings.py
+++ project_amonze/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_ouguz$-eyy06b1!r0x4s+9axphhdk)x_(r3s_aiu@49b4@yb$'
+SECRET_KEY = 'abc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
